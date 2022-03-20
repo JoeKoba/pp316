c9750f34d6639dcf5118a7b791dcd81b268651a5
maxim <maxim@debian-pc.maxim>
2022-03-20 11:02:27 +0300

    Task 315

---
+++ src/main/java/ru/alexeykedr/config/Config.java
@@ -0,0 +1,9 @@
+package ru.alexeykedr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.alexeykedr")
+public class Config {
+}
